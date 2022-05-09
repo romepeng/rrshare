d7ca9a06b432de3d150c9362b51734c72d0a3cc5
romepeng <romepeng@outlook.com>
2022-05-09 14:08:03 +0800

    Update fetch_swl1_index_class_and_realtime.py

--- rrshare/rqFetch/fetch_swl1_index_class_and_realtime.py
+++ rrshare/rqFetch/fetch_swl1_index_class_and_realtime.py
@@ -6,6 +6,58 @@ import pandas as pd
 
 from rrshare.rqUtil import rq_util_get_last_tradedate, rq_util_if_trade, rq_util_if_tradetime, rq_util_date_today
 
+
+
+SWL1_INDEX = ('801010','801030','801040','801050','801080','801110','801120','801130','801140','801150','801160','801170','801180','801200','801210', \
+              '801230','801710','801720','801730','801740','801750','801760','801770','801780','801790','801880','801890','801950','801960','801970','801980')
+
+# sw-cons
+sw_cons_headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "Connection": "keep-alive",
+    "Content-Length": "34",
+    "Content-Type": "text/plain; charset=UTF-8",
+    # "Cookie": "ASP.NET_SessionId=i55eaz55142xdxfx0bkqp145",
+    "Host": "www.swsindex.com",
+    "Origin": "http://www.swsindex.com",
+    "Referer": "http://www.swsindex.com/idx0210.aspx?swindexcode=801010",
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+    "X-AjaxPro-Method": "ReturnContent",
+}
+
+# sw-url
+sw_url = "http://www.swsindex.com/handler.aspx"
+
+# sw-payload
+sw_payload = {
+    "tablename": "swzs",
+    "key": "L1",
+    "p": "1",
+    "where": "L1 in('801010','801020','801030','801040','801050','801080','801110','801120','801130','801140','801150','801160','801170','801180','801200','801210','801230','801710','801720','801730','801740','801750','801760','801770','801780','801790','801880','801890','801950','801960','801970','801980')",
+    "orderby": "",
+    "fieldlist": "L1,L2,L3,L4,L5,L6,L7,L8,L11",
+    "pagecount": "28",
+    "timed": "",
+}
+
+# sw-headers
+sw_headers = {
+    'Accept': 'application/json, text/javascript, */*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'DNT': '1',
+    'Host': 'www.swsindex.com',
+    'Origin': 'http://www.swsindex.com',
+    'Pragma': 'no-cache',
+    'Referer': 'http://www.swsindex.com/idx0120.aspx?columnid=8832',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
 class Swsindex(object):
     """http://www.swsindex.com/idx0120.aspx?columnid=8832#
     industry class:
@@ -39,9 +91,7 @@ class Swsindex(object):
             'tablename': 'swzs',
             'key': 'L1',
             'p': '{}'.format(i),
-            'where': "L1 in('801010','801020','801030','801040','801050','801080','801110','801120','801130','801140', \
-                            '801150','801160','801170','801180','801200','801210','801230','801710','801720','801730',\
-                            '801740','801750','801760','801770','801780','801790','801880','801890')",
+            'where': "L1 in SWL1_INDEX,
             'orderby': '',
             'fieldlist': 'L1,L2,L3,L4,L5,L6,L7,L8,L11',
             'pagecount': '28',
@@ -116,9 +166,7 @@ class Swsindex(object):
 
 
     def get_swl1_class(self):
-        L1_INDEX = ['801010','801020','801030','801040','801050','801080','801110','801120','801130','801140', \
-                            '801150','801160','801170','801180','801200','801210','801230','801710','801720','801730',\
-                            '801740','801750','801760','801770','801780','801790','801880','801890']
+        L1_INDEX = list(SWL1_INDEX)
         df = pd.DataFrame()
         for l in L1_INDEX:
             print(f'index code: {l}')
